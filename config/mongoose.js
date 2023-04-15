cff1ba01ed9d003f4903e39493d2038c6f9a63ab
Prafulla Dhake <prafulladhake@gmail.com>
2023-04-16 01:57:55 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://127.0.0.1:27017/PollingSystemAPI");
+
+
+const db= mongoose.connection;
+db.on("error", console.error.bind(console,"Error in connection to Mongodb"));
+
+
+db.once("open", function(){
+    console.log("Successfully connected to Database :: MongoDB");
+
+    
+});
+
+module.exports =db;
+
